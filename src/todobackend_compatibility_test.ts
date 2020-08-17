7e022b919138fa368da7432fbcf82319b1253613
Duong Pham <tanphamhaiduong@gmail.com>
2020-08-17 22:47:50 +0700

    chore: add todobackend compatibility

--- src/todobackend_compatibility_test.ts
+++ src/todobackend_compatibility_test.ts
@@ -1,25 +1,28 @@
 import Servirtium, { IServirtium } from './servirtium'
 
-var myArgs = process.argv.slice(2);
 
-var servirtium
+const run = async() => {
+    let myArgs = process.argv.slice(2);
 
-switch (myArgs[0]) {
-    case 'record':
-        servirtium = new Servirtium('https://todo-backend-sinatra.herokuapp.com')
-        servirtium.setTestName("todobackend_test_suite.md")
-        servirtium.startRecord(() => {
-            // ?
-        })
-        break;
-    case 'playback':
-        servirtium = new Servirtium('https://todo-backend-sinatra.herokuapp.com')
-        servirtium.setTestName("todobackend_test_suite.md")
-        servirtium.startPlayback(() => {
-            // ?
-        })
-        break;
-    default:
-        console.log('Oops, should have been record or playback');
+    let servirtium: IServirtium
+
+    switch (myArgs[0]) {
+        case 'record':
+            servirtium = new Servirtium('https://todo-backend-sinatra.herokuapp.com')
+            servirtium.setTestName("todobackend_test_suite.md")
+            await servirtium.startRecord()
+            await servirtium.writeRecord()
+            await servirtium.endRecord()
+            break;
+        case 'playback':
+            servirtium = new Servirtium('https://todo-backend-sinatra.herokuapp.com')
+            servirtium.setTestName("todobackend_test_suite.md")
+            await servirtium.startPlayback()
+            await servirtium.endPlayback()
+            break;
+        default:
+            console.log('Oops, should have been record or playback');
+    }
 }
 
+run()
