7079b843978fad0110e9c5efa826f07f24a0ac3a
Paul Hammant <paul@hammant.org>
2022-08-21 12:40:12 +0100

    work on compatibility suite

---
+++ src/chllenge_tests.js
@@ -0,0 +1,10 @@
+//import Challenge from "./challenge.js";
+
+describe('something1', () => {
+    describe('something2', () => {
+        it('should something3 else', () => {
+            expect(2 + 2 ).toEqual(5)
+        })
+    })
+})
+
